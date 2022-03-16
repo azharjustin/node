8c67f677347469cb9479d10e94852de43a3ac6a4
azharjustin <azharjustin37@gmail.com>
2022-03-16 12:11:18 -0700

    heroke

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { MongoClient } from "mongodb";
 dotenv.config();
 console.log(process.env.MONGO_URL)
 const app = express();
-const PORT = 4000;
+// const PORT = 4000;
+const PORT = process.env.PORT;
 const movies = [{
     "id": "100",
     "name": "Iron man 2",
